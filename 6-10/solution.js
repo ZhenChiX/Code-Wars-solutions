a854e3d0888937b012aa75014ba68112218e7fdf
StarGale <zhenchix@hotmail.com>
2018-06-13 15:12:24 -0700

    fix solution

--- 6-10/solution.js
+++ 6-10/solution.js
@@ -1,12 +1,1 @@
-function isNice(arr) {
-	let niceArr = false;
-
-	for (var i in arr) {
-		if (arr.includes(arr[i] + 1) || arr.includes(arr[i] - 1)) {
-			niceArr = true;
-		} else {
-			return false;
-		}
-	}
-	return niceArr;
-}
+let disemvowel = str => str.replace(/[AEIOU]/ig,'');
